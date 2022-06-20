10770d7b4727f775ead2dba61d047dc23101ac46
Aec <alpemre.can@gmail.com>
2022-06-20 20:58:33 +0300

    proje oluşturuldu

---
+++ src/main/java/com/alpemre/firsthomework/FirsthomeworkApplication.java
@@ -0,0 +1,14 @@
+package com.alpemre.firsthomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirsthomeworkApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirsthomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alpemre/firsthomework/FirsthomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpemre.firsthomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirsthomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
